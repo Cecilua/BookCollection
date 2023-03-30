7cce917230fb06d5b64e2841dde278519482838b
Cecilua <65154517+Cecilua@users.noreply.github.com>
2023-03-31 11:10:33 +1300

    Innitial commit -- Book class

---
+++ Book.java
@@ -0,0 +1,43 @@
+
+/**
+ * support class Book
+ * book has an title, author, genre, publishing year, no. pages, and a cover  
+ *
+ * @author CC
+ * @version 30/03/23
+ */
+public class Book
+{
+    // instance variables - replace the example below with your own
+    
+    private String title;
+    private String author;
+    private String genre;
+    private int published; 
+    private int pages;
+    private String image;
+    private static final String DEFAULTIMG = "default_book_cover.jpeg";
+
+    /**
+     * Constructor for objects of class Book
+     */
+    public Book(String ttl, String auth, String gnr, int publish, int pgs, String img)
+    {
+        // initialise instance variables
+        this.title = ttl;
+        this.author = auth;
+        this.genre = gnr;
+        this.published = publish;
+        this.pages = pgs;
+        this.image = img;
+    }
+    
+    /**
+     * Constructor for objects of class Book
+     */
+    public Book(String ttl, String auth, String gnr, int publish, int pgs)
+    {
+        // initialise instance variables       
+        this(ttl, auth, gnr, publish, pgs, DEFAULTIMG);
+    }
+}
